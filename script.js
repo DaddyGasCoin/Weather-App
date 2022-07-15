c6ad06ed3e319729a4bb8e6eb7e589305b948a71
Michael Jones <iammichaeljon27@gmail.com>
2022-07-15 08:55:58 +0400

    Add validation for inputs and catch error from API calls

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 
 const key = '85f888c422acb6c020449e2983453f27'
-let country = ''
-async function getweather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${key}&units=metric`)
-    const json = await (response.json())
-    const description = await json.weather[0].description
-    const type = json.weather[0].main
-    const humidity = json.main.humidity
-    const feelLike = json.main.feels_like
-    const pressure = json.main.pressure
-    const temp = json.main.temp
-    const maxtemp = json.main.temp_max
-    const mintemp = json.main.temp_min
-    const detaills = [description, type, humidity, feelLike, pressure, temp, maxtemp, mintemp]
-    printdeatails(detaills)
-}
-const x = document.querySelector('#country')
+const input = document.querySelector('#country')
 const btn = document.querySelector('button')
 const loader = document.querySelector('.loader')
+const errorspan = document.querySelector('.error')
+let country = ''
+
+async function getweather() {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${key}&units=metric`)
+        const json = await (response.json())
+        const description = await json.weather[0].description
+        const type = json.weather[0].main
+        const humidity = json.main.humidity
+        const feelLike = json.main.feels_like
+        const pressure = json.main.pressure
+        const temp = json.main.temp
+        const maxtemp = json.main.temp_max
+        const mintemp = json.main.temp_min
+        const detaills = [description, type, humidity, feelLike, pressure, temp, maxtemp, mintemp]
+        printdeatails(detaills)
+    }
+    catch (error) {
+        errorspan.textContent = 'Please enter valid input'
+        input.classList.add('invalid')
+        loader.children[0].remove()
+    }
+}
 
 
 function printdeatails(arr) {
@@ -36,15 +45,29 @@ function printdeatails(arr) {
     pressure.textContent = arr[4]
 
     loader.children[0].remove()
-
+    errorspan.textContent = ''
+    input.classList.remove('invalid')
 
 }
 
+function showerror() {
+
+    if (input.validity.valueMissing) {
+        errorspan.textContent = 'This is a required field'
+        input.classList.add('invalid')
+    }
+}
+
 btn.addEventListener('click', () => {
-    country = x.value
-    let img = document.createElement('img')
-    img.src = 'autorenew_black_24dp.svg'
-    img.classList.add('image')
-    loader.appendChild(img)
-    getweather()
+    if (!input.validity.valid) {
+        showerror()
+    }
+    else {
+        country = input.value
+        let img = document.createElement('img')
+        img.src = 'autorenew_black_24dp.svg'
+        img.classList.add('image')
+        loader.appendChild(img)
+        getweather()
+    }
 })
